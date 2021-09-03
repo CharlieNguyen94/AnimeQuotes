14367d0ca9711c82da63d91c6be30f179cfb034c
CharlieNguyen94 <charlietugnuyen@hotmail.com>
2021-09-03 10:23:54 +0100

    Static constant for BaseURL.

---
+++ AnimeQuotes/Constants/APIConstants.swift
@@ -0,0 +1,12 @@
+//
+//  APIConstants.swift
+//  APIConstants
+//
+//  Created by Charlie Nguyen on 03/09/2021.
+//
+
+import Foundation
+
+enum APIConstants {
+    static let baseURL = "https://animechan.vercel.app"
+}
