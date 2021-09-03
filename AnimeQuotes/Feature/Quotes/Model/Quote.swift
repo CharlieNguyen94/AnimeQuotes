bae279d8ab7a025aef66458750637f0a9c319936
CharlieNguyen94 <charlietugnuyen@hotmail.com>
2021-09-03 10:30:52 +0100

    Model for quote.

---
+++ AnimeQuotes/Feature/Quotes/Model/Quote.swift
@@ -0,0 +1,14 @@
+//
+//  Quote.swift
+//  Quote
+//
+//  Created by Charlie Nguyen on 03/09/2021.
+//
+
+import Foundation
+
+struct Quote: Decodable {
+    let anime: String
+    let character: String
+    let quote: String
+}
