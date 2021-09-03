c44e1b50e0cec78525fe65dfa38d52229483ad7a
CharlieNguyen94 <charlietugnuyen@hotmail.com>
2021-09-03 10:15:42 +0100

    Initial Commit

---
+++ AnimeQuotes/AnimeQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeQuotesApp.swift
+//  AnimeQuotes
+//
+//  Created by Charlie Nguyen on 03/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimeQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
